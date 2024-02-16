9e42ec42b7d1a81fa53c65ec8471e53915d90dee
voojudev <HM@vooju.com>
2024-02-16 11:32:02 +0800

    compat bugfix for mysql virtual driver postfixadmin vacation table use 'active' column with tinyint(1) while postgres version of postfixadmin vacation table use 'active' column with true/false bool

--- lib/virtual.class.php
+++ lib/virtual.class.php
@@ -128,12 +128,22 @@ class Virtual extends VacationDriver {
 	      if ($this->enable == '') {
             $this->enable = 0;
         }
+
+        //mysql version of postfixadmin vacation table use 'active' column with tinyint(1)
+        //pgsql version of postfixadmin vacation table use 'active' column with true/false bool
+        $enable_status = $this->enable;
+        if($this->db_is_postgres) {
+          $enable_status = $this->enable ? 'TRUE' : 'FALSE';
+        }else{
+          $enable_status = $this->enable ? '1' : '0';
+        }
+
         $this->db->query($sql, 
 	          rcube::Q($this->user->data['username']), 
 	          $this->subject, 
 	          $this->body,
 	          $this->domain,
-	          $this->enable ? 'TRUE' : 'FALSE',
+	          $enable_status,
 	          rcube::Q($this->user->data['username']));
         if ($error = $this->db->is_error()) {
             if (strpos($error, "no such field")) {
